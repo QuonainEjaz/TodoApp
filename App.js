b40d8e9df2fc37c5656854adb14faa9904538a85
QuonainEjaz <QuonainEjazwhetstonez@gmail.com>
2025-01-20 16:55:58 +0500

    initial commit

---
+++ App.js
@@ -0,0 +1,195 @@
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, TextInput, Button, View, FlatList, Text, Keyboard, Alert } from 'react-native';
+import TodoItem from './components/TodoItem';
+
+
+export default function App() {
+  // This state variable holds the list of todos.
+  // It is initialized to an empty array.
+  const [todos, setTodos] = useState([]);
+
+  // This state variable holds the text of the title and description
+  // of the new todo that the user is currently entering.
+  // It is initialized to empty strings.
+  const [input, setInput] = useState({ title: '', desc: '' });
+
+  // This state variable holds a boolean value that indicates
+  // whether the user is currently editing an existing todo.
+  // It is initialized to false.
+  const [isEditing, setIsEditing] = useState(false);
+
+  // This state variable holds the id of the todo that the user is
+  // currently editing.
+  // If the user is not editing any todo, it is null.
+  // It is initialized to null.
+  const [editingId, setEditingId] = useState(null);
+
+  // This function changes the title of the input state variable.
+  // It is called when the user changes the text in the title input field.
+  const handleTitleChange = (text) => {
+    setInput({ ...input, title: text });
+  };
+
+  // This function changes the description of the input state variable.
+  // It is called when the user changes the text in the description input field.
+  const handleDescChange = (text) => {
+    setInput({ ...input, desc: text });
+  };
+
+  // This function adds a new todo to the list of todos.
+  // If the user is editing an existing todo, it updates the existing todo.
+  // It is called when the user presses the "ADD TODO" button.
+  const handleAddTodo = () => {
+    // Dismiss the keyboard.
+    Keyboard.dismiss();
+
+    // If the title is empty, display an alert and do nothing else.
+    if ((input.title === undefined) || (input.title.trim() === '')) {
+      Alert.alert('Error', 'Title cannot be empty');
+      return;
+    }
+
+    // If the user is editing an existing todo, update the existing todo.
+    if (isEditing) {
+      // If the description is empty, set it to 'None'.
+      if ((input.desc === undefined) || (input.desc.trim() === '')) {
+        input.desc = 'None';
+      };
+      // Update the existing todo in the list of todos.
+      setTodos(
+        (itTakesAllTodosAssArray) =>
+          itTakesAllTodosAssArray.map((mappingEachTodo) =>
+            mappingEachTodo.id === editingId
+              ? { ...mappingEachTodo, title: input.title, desc: input.desc }
+              : mappingEachTodo
+          )
+      );
+      // Set isEditing to false to indicate that the user is not editing any todo.
+      setIsEditing(false);
+      // Set editingId to null to indicate that the user is not editing any todo.
+      setEditingId(null);
+    } else {
+      // Add a new todo to the list of todos.
+      setTodos([...todos, { id: Date.now().toString(), title: input.title, desc: input.desc }]);
+    }
+    // If the description is empty, set it to 'None'.
+    if ((input.desc === undefined) || (input.desc.trim() === '')) {
+      input.desc = 'None';
+      setTodos([...todos, { id: Date.now().toString(), title: input.title, desc: input.desc }]);
+    };
+    // Reset the input state variable to empty strings.
+    setInput('');
+  };
+
+  // This function edits an existing todo.
+  // It is called when the user presses the "EDIT" button.
+  const handleEditTodo = (id, todo) => {
+    // Dismiss the keyboard.
+    Keyboard.dismiss();
+
+    // If the description of the todo is 'None', set the description of the input state variable to an empty string.
+    if (todo.desc === 'None') {
+      setInput({ title: todo.title });
+      setIsEditing(true);
+      setEditingId(id);
+      return true;
+    };
+
+    // Set the title and description of the input state variable to the title and description of the todo.
+    setInput({ title: todo.title, desc: todo.desc });
+    setIsEditing(true);
+    setEditingId(id);
+  };
+
+  // This function deletes an existing todo.
+  // It is called when the user presses the "DELETE" button.
+  const handleDeleteTodo = (id) => {
+    // Dismiss the keyboard.
+    Keyboard.dismiss();
+
+    // Remove the todo from the list of todos.
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    // Set isEditing to false to indicate that the user is not editing any todo.
+    setIsEditing(false);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Todo List</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputTitle}
+          placeholder="Title:"
+          maxLength={30}
+          value={input.title}
+          onChangeText={handleTitleChange}
+        />
+        <TextInput
+          style={styles.inputDesc}
+          placeholder="Description (optional):"
+          multiline={true}
+          value={input.desc}
+          onChangeText={handleDescChange}
+        />
+      </View>
+      <Button
+        title={isEditing ? 'UPDATE' : 'ADD TODO'}
+        onPress={handleAddTodo}
+        color="#1976D2"
+      />
+      <FlatList
+        data={todos}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TodoItem
+            todo={item}
+            onEdit={handleEditTodo}
+            onDelete={handleDeleteTodo}
+          />
+        )}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightyellow',
+    padding: 20,
+
+
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#4A4A4A',
+    marginTop: 10,
+    marginBottom: 20,
+    textAlign: 'center',
+    
+  },
+  inputContainer: {
+    marginBottom: 15,
+    alignItems: 'center',
+  },
+  inputTitle: {
+    borderWidth: 1,
+    borderColor: 'green',
+    borderRadius: 5,
+    paddingLeft: 20,
+    marginBottom: 5,
+    backgroundColor: '#FFFAF0',
+    width: 380,
+    height: 50,
+  },
+  inputDesc: {
+    borderWidth: 1,
+    borderColor: 'green',
+    borderRadius: 5,
+    padding: 20,
+    backgroundColor: '#FFFAF0',
+    width: 380,
+    height: 100,
+  },
+});
---
+++ components/TodoItem.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
+export default function TodoItem({
+  // The todo object that we are currently rendering.
+  todo,
+  // A function that is called when the user presses the "EDIT" button.
+  // It is given the id of the todo that the user is pressing the button for.
+  onEdit,
+  // A function that is called when the user presses the "DELETE" button.
+  // It is given the id of the todo that the user is pressing the button for.
+  onDelete,
+  // A boolean that indicates whether the checkbox next to the todo should be checked.
+  isChecked,
+  // A function that is called when the user checks the checkbox next to the todo.
+  // It is given a boolean that indicates the new state of the checkbox.
+  setIsChecked,
+}) {
+  return (
+    <View style={styles.todoContainer}>
+      {/* The left side of the todo item. */}
+      <View>
+        {/* The title of the todo. */}
+        <Text style={styles.todoTitle}>{todo.title}</Text>
+        {/* The description of the todo. */}
+        <Text style={styles.desc}>{todo.desc}</Text>
+      </View>
+      {/* The right side of the todo item. */}
+      <View style={styles.buttonsContainer}>
+        {/* The "DELETE" button. */}
+        <Button
+          title="DELETE"
+          color="#8E24AA"
+          onPress={() => onDelete(todo.id)}
+        />
+        {/* The "EDIT" button. */}
+        <Button
+          title="EDIT"
+          color="#388E3C"
+          onPress={() => onEdit(todo.id, todo)}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  todoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: '#FFF',
+    padding: 15,
+    borderRadius: 5,
+    marginTop: 10,
+    borderWidth: 2,
+    borderColor: 'green',
+  },
+  todoTitle: {
+    textTransform: 'capitalize',
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+    width: 340,
+    marginTop: -10,
+    marginBottom: 15,
+  },
+  buttonsContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 240,
+    margin: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 115,
+  },
+  desc: {
+    fontSize: 12,
+    color: '#999',
+    width: 590,
+    height: 50,
+    marginBottom: 15,
+  },
+});
